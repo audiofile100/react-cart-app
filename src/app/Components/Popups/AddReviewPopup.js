e5490fdfde39ed9771802a788e96d3380f08a305
audiofile100 <amplified100@gmail.com>
2021-02-14 10:58:32 -0700

    review popup form

--- src/app/Components/Popups/AddReviewPopup.js
+++ src/app/Components/Popups/AddReviewPopup.js
@@ -6,7 +6,12 @@ class AddReviewPopup extends Component {
         return (
             <div className="popup">
                 <div className="popup_inner">
-                    <h1>{this.props.text}</h1>
+                    <h5>Enter your reviews</h5>
+                    <input type="text" className="form-control col-md-6 username" placeholder="Name/Alias (optional)" />
+                    <textarea rows="10" ></textarea>
+                    <p>Rating</p>
+                    <input type="number" min="1" max="5" defaultValue="1" className="form-control col-md-2 username" />
+                    <br/>
                     <button onClick={this.props.closePopup}>Done</button>
                 </div>
             </div>
