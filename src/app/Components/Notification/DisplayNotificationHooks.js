9b09e343b9d8874f6aebf938cd20b98aa3fc462b
audiofile100 <amplified100@gmail.com>
2021-02-26 12:20:17 -0700

    click event dismiss modal

--- src/app/Components/Notification/DisplayNotificationHooks.js
+++ src/app/Components/Notification/DisplayNotificationHooks.js
@@ -1,19 +1,18 @@
 "use strict";
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 import NotificationCard from '../Notification/NotificationCard';
 
 let NotificationComponentHooks = (props) => {
 
     return(
-        <div className="notification">
+        <div className="notification" onClick={props.toggle}>
             <div className="notification_inner">
                 <NotificationCard clickedCard={props.clickedCard} title="Add Product" description="To add products go to product page"/>
                 <NotificationCard clickedCard={props.clickedCard} title="Add Item" description="To add items to cart go to product page" />
                 <NotificationCard clickedCard={props.clickedCard} title="Review Cart" description="To review your cart go to cart page" />
                 <NotificationCard clickedCard={props.clickedCard} title="Payments" description="To make payment go to checkout in cart"/>
-                <NotificationCard clickedCard={props.clickedCard} title="Cancelling Orders" description="To cancel order go to orders page"/>
-                <button onClick={props.toggle}>Close</button>
+                <NotificationCard clickedCard={props.clickedCard} title="Cancelling Orders" description="To cancel orders go to the orders page"/>
             </div>
         </div>
     )
